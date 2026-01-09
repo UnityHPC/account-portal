fe0a2f814b94b8d5a0265a7b52e480ce0ce1f415
Simon Leary <simon.leary42@proton.me>
2026-01-09 12:00:13 -0500

    rewrite test to not require dedicated user nicknames

--- test/functional/DefunctUnsetOrFalseTest.php
+++ test/functional/DefunctUnsetOrFalseTest.php
@@ -4,14 +4,24 @@ class DefunctUnsetOrFalseTest extends UnityWebPortalTestCase
 {
     public function testPIMgmtShowsBothGroupsWithDefunctAttributeSetFalseAndUnset()
     {
-        global $USER;
-        $this->switchUser("PIDefunctAttributeUnset");
-        $gidWithAttributeUnset = $USER->getPIGroup()->gid;
-        $this->switchUser("PIDefunctAttributeSetFalse");
-        $gidWithAttributeSetFalse = $USER->getPIGroup()->gid;
-        $this->switchUser("Admin");
-        $output = http_get(__DIR__ . "/../../webroot/admin/pi-mgmt.php");
-        $this->assertStringContainsString($gidWithAttributeUnset, $output);
-        $this->assertStringContainsString($gidWithAttributeSetFalse, $output);
+        global $USER, $LDAP;
+        $this->switchUser("NormalPI");
+        $pi_group = $USER->getPIGroup();
+        $entry = $LDAP->getPIGroupEntry($pi_group->gid);
+        $this->assertEquals([], $entry->getAttribute("isDefunct"));
+        $this->assertStringContainsString(
+            $pi_group->gid,
+            http_get(__DIR__ . "/../../webroot/admin/pi-mgmt.php"),
+        );
+        try {
+            $pi_group->setIsDefunct(false);
+            $this->assertEquals(["FALSE"], $entry->getAttribute("isDefunct"));
+            $this->assertStringContainsString(
+                $pi_group->gid,
+                http_get(__DIR__ . "/../../webroot/admin/pi-mgmt.php"),
+            );
+        } finally {
+            $entry->removeAttribute("isDefunct");
+        }
     }
 }
