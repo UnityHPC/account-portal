57ce0c02060e59750d6c083cdcb83ebb380270b3
Simon Leary <simon.leary42@proton.me>
2026-01-29 16:57:50 -0500

    override HTTP_HOST in executeWorker

--- workers/init.php
+++ workers/init.php
@@ -1,9 +1,17 @@
 <?php
 if (!array_key_exists("HTTP_HOST", $_SERVER)) {
-    $_SERVER["HTTP_HOST"] = "worker"; // see deployment/overrides/worker
+    if (getenv("HTTP_HOST")) {
+        $_SERVER["HTTP_HOST"] = getenv("HTTP_HOST");
+    } else {
+        $_SERVER["HTTP_HOST"] = "worker"; // see deployment/overrides/worker
+    }
 }
 if (!array_key_exists("REMOTE_ADDR", $_SERVER)) {
-    $_SERVER["REMOTE_ADDR"] = "127.0.0.1"; // needed for audit log
+    if (getenv("REMOTE_ADDR")) {
+        $_SERVER["REMOTE_ADDR"] = getenv("REMOTE_ADDR");
+    } else {
+        $_SERVER["REMOTE_ADDR"] = "127.0.0.1"; // needed for audit log
+    }
 }
 
 require_once __DIR__ . "/../resources/autoload.php";
