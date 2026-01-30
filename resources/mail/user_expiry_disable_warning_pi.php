95bcc99c77915c5fbc629df816f01da4b415e6f1
Simon Leary <simon.leary42@proton.me>
2026-01-30 14:04:13 -0500

    link to account policy in warning emails

--- resources/mail/user_expiry_disable_warning_pi.php
+++ resources/mail/user_expiry_disable_warning_pi.php
@@ -6,8 +6,9 @@ $this->Subject = "Account Expiration Warning";
 $idle_days = $data["idle_days"];
 $expiration_date = $data["expiration_date"];
 $is_final_warning = $data["is_final_warning"];
+$portal_hyperlink = getHyperlink("account portal");
+$policy_hyperlink = getHyperlink("account policy", CONFIG["site"]["account_policy_url"]);
 $pi_group_gid = $data["pi_group_gid"];
-$hyperlink = getHyperlink("account portal");
 
 echo "
     Your account and PI group are scheduled to be disabled on $expiration_date because you have been idle for too long.
@@ -15,7 +16,8 @@ echo "
     you will lose access to UnityHPC Platform services,
     and your group members also may lose access unless they are a member of some other group.
     If you don't wish for this to happen,
-    reset the inactivity timer by simply logging in to the $hyperlink.
+    reset the inactivity timer by simply logging in to the $portal_hyperlink.
+    For more information, see our $policy_hyperlink.
 ";
 if ($is_final_warning) {
     echo "This is the final warning.\n";
