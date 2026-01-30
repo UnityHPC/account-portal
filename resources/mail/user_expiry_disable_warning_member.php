9a625e7387d22ac79bc078eec5938b4c6973a2f0
Simon Leary <simon.leary42@proton.me>
2026-01-30 14:04:13 -0500

    link to account policy in warning emails

--- resources/mail/user_expiry_disable_warning_member.php
+++ resources/mail/user_expiry_disable_warning_member.php
@@ -6,15 +6,17 @@ $this->Subject = "PI Group Expiration Warning";
 $idle_days = $data["idle_days"];
 $expiration_date = $data["expiration_date"];
 $is_final_warning = $data["is_final_warning"];
+$portal_hyperlink = getHyperlink("account portal");
+$policy_hyperlink = getHyperlink("account policy", CONFIG["site"]["account_policy_url"]);
 $pi_group_gid = $data["pi_group_gid"];
-$hyperlink = getHyperlink("account portal");
 
 echo "
     The PI group $pi_group_gid is scheduled to be disabled on $expiration_date because the group owner has been idle for too long.
     Upon expiration, this group's files will be permanently deleted,
     and you will lose access to UnityHPC Platform services unless you are a member of any other PI group.
     If you don't wish for this to happen,
-    remind your PI to reset the inactivity timer by simply logging in to the $hyperlink.
+    remind your PI to reset the inactivity timer by simply logging in to the $portal_hyperlink.
+    For more information, see our $policy_hyperlink.
 ";
 if ($is_final_warning) {
     echo "This is the final warning.\n";
--- resources/mail/user_expiry_disable_warning_non_pi.php
+++ resources/mail/user_expiry_disable_warning_non_pi.php
@@ -6,13 +6,15 @@ $this->Subject = "Account Expiration Warning";
 $idle_days = $data["idle_days"];
 $expiration_date = $data["expiration_date"];
 $is_final_warning = $data["is_final_warning"];
-$hyperlink = getHyperlink("account portal");
+$portal_hyperlink = getHyperlink("account portal");
+$policy_hyperlink = getHyperlink("account policy", CONFIG["site"]["account_policy_url"]);
 
 echo "
     Your account is scheduled to be disabled on $expiration_date because you have been idle for too long.
     Upon expiration, you will lose access to UnityHPC Platform services.
     If you don't wish for this to happen,
-    reset the inactivity timer by simply logging in to the $hyperlink.
+    reset the inactivity timer by simply logging in to the $portal_hyperlink.
+    For more information, see our $policy_hyperlink.
 ";
 if ($is_final_warning) {
     echo "This is the final warning.\n";
