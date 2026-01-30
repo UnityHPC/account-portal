ae1011e5cb7c47f0cc22ac3956277b46dfebec18
Simon Leary <simon.leary42@proton.me>
2026-01-30 11:21:21 -0500

    update mail templates

--- resources/mail/user_expiry_idlelock_warning.php
+++ resources/mail/user_expiry_idlelock_warning.php
@@ -6,14 +6,15 @@ $this->Subject = "Account Expiration Warning";
 $idle_days = $data["idle_days"];
 $expiration_date = $data["expiration_date"];
 $is_final_warning = $data["is_final_warning"];
+$hyperlink = getHyperlink("account portal");
 
-echo "Your account is set to be disabled on $expiration_date because you have been idle for too long.\n";
+echo "
+    Your account is set to be disabled on $expiration_date because you have been idle for too long.
+    Upon expiration, you will lose access to UnityHPC Platform services
+    until you reset the inactivity timer by logging in to the $hyperlink.
+";
 if ($is_final_warning) {
     echo "This is the final warning.\n";
 }
 ?>
-<p>
-Upon expiration, you will lose access to UnityHPC Platform services until you reset the inactivity timer by logging in to the <?php echo getHyperlink(
-    "Unity account portal",
-); ?>.
 </p>
