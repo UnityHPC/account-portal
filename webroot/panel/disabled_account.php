77ef43fb4727aad7fda22d5c742dc25d5f6f5f65
Simon Leary <simon.leary42@proton.me>
2026-02-04 12:34:37 -0500

    fix bugs, prettier

--- webroot/panel/disabled_account.php
+++ webroot/panel/disabled_account.php
@@ -16,16 +16,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 require getTemplatePath("header.php");
 $CSRFTokenHiddenFormInput = UnityHTTPD::getCSRFTokenHiddenFormInput();
-$account_policy_hyperlink = getHyperlink("account policy", CONFIG["site"]["account_policy_url"]);
+$account_policy_url = CONFIG["site"]["account_policy_url"];
 $support_mail = CONFIG["mail"]["support"];
 ?>
 <h1>Disabled Account</h1>
 <hr>
-<p>
+<p style="text-wrap: balance;">
     Your account has been disabled, but you can re-enable it.
-    Accounts are disabled automatically according to our <?php echo $account_policy_hyperlink; ?>.
-    If this is unexpected, <a href="$support_mail">send us an email</a>.
+    Accounts are disabled automatically according to our <a href="<?php echo $account_policy_url; ?>">account policy</a>.
+    If this is unexpected, <a href="mailto:<?php echo $support_mail; ?>">send us an email</a>.
 </p>
+<br>
 <p>Please verify that the information below is correct before continuing:</p>
 <div>
     <strong>Name&nbsp;&nbsp;</strong>
