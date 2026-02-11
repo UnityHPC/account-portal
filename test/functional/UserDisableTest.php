0e730b319c7dbedf6e414000ba17e2d9a6f57e3a
Simon Leary <simon.leary42@proton.me>
2026-02-11 11:22:51 -0500

    add more test cases

--- test/functional/UserDisableTest.php
+++ test/functional/UserDisableTest.php
@@ -1,4 +1,5 @@
 <?php
+use UnityWebPortal\lib\UnityHTTPDMessageLevel;
 use UnityWebPortal\lib\UserFlag;
 
 class UserDisableTest extends UnityWebPortalTestCase
@@ -6,13 +7,49 @@ class UserDisableTest extends UnityWebPortalTestCase
     public function testDisableUser()
     {
         global $USER;
-        $this->switchUser("Normal");
+        $this->switchUser("Blank");
         $this->assertFalse($USER->getFlag(UserFlag::DISABLED));
         try {
             http_post(__DIR__ . "/../../webroot/panel/account.php", [
                 "form_type" => "disable",
             ]);
             $this->assertTrue($USER->getFlag(UserFlag::DISABLED));
+        } finally {
+            if ($USER->getFlag(UserFlag::DISABLED)) {
+                $USER->reEnable();
+            }
+        }
+    }
+
+    public function testDisableUserButIsPI()
+    {
+        global $USER;
+        $this->switchUser("EmptyPIGroupOwner");
+        $this->assertFalse($USER->getFlag(UserFlag::DISABLED));
+        try {
+            http_post(__DIR__ . "/../../webroot/panel/account.php", [
+                "form_type" => "disable",
+            ]);
+            $this->assertFalse($USER->getFlag(UserFlag::DISABLED));
+            $this->assertMessageExists(UnityHTTPDMessageLevel::ERROR, "/.*/", "/You are a PI/");
+        } finally {
+            if ($USER->getFlag(UserFlag::DISABLED)) {
+                $USER->reEnable();
+            }
+        }
+    }
+
+    public function testDisableUserButIsPIGroupMember()
+    {
+        global $USER;
+        $this->switchUser("EmptyPIGroupOwner");
+        $this->assertFalse($USER->getFlag(UserFlag::DISABLED));
+        try {
+            http_post(__DIR__ . "/../../webroot/panel/account.php", [
+                "form_type" => "disable",
+            ]);
+            $this->assertFalse($USER->getFlag(UserFlag::DISABLED));
+            $this->assertMessageExists(UnityHTTPDMessageLevel::ERROR, "/.*/", "/you are a member/");
         } finally {
             if ($USER->getFlag(UserFlag::DISABLED)) {
                 $USER->reEnable();
