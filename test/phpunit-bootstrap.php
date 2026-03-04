958fc947ad5bf7f6fb73e59361243de244dd7572
Simon Leary <simon.leary42@proton.me>
2026-03-04 15:24:55 -0500

    public

--- test/phpunit-bootstrap.php
+++ test/phpunit-bootstrap.php
@@ -209,7 +209,7 @@ class UnityWebPortalTestCase extends TestCase
     private ?string $current_user_nickname = null;
     private array $nickname_to_latest_session_id = [];
     // FIXME these names are wrong
-    protected static array $UID2ATTRIBUTES = [
+    public static array $UID2ATTRIBUTES = [
         "user1_org1_test" => ["user1@org1.test", "foo", "bar", "user1@org1.test"],
         "user2_org1_test" => ["user2@org1.test", "foo", "bar", "user2@org1.test"],
         "user3_org1_test" => ["user3@org1.test", "foo", "bar", "user3@org1.test"],
@@ -235,7 +235,7 @@ class UnityWebPortalTestCase extends TestCase
             "user1+cs123_org1_test@org1.test",
         ],
     ];
-    protected static array $NICKNAME2UID = [
+    public static array $NICKNAME2UID = [
         "Admin" => "user1_org1_test",
         "Blank" => "user2_org1_test",
         "EmptyPIGroupOwner" => "user5_org2_test",
@@ -257,7 +257,7 @@ class UnityWebPortalTestCase extends TestCase
         "ImmortalNotPI" => "user13_org1_test",
     ];
 
-    protected function validateUser(string $nickname)
+    public function validateUser(string $nickname)
     {
         global $USER, $SQL, $LDAP;
         if (!array_key_exists($nickname, self::$NICKNAME2UID)) {
@@ -675,7 +675,7 @@ class UnityWebPortalTestCase extends TestCase
         return TRegxDataProvider::list(...self::findPHPFiles(__DIR__ . "/../webroot/admin"));
     }
 
-    public static function panelPagesWithNoSpecialRedirects()
+    private static function panelPagesWithNoSpecialRedirects()
     {
         $panel = __DIR__ . "/../webroot/panel";
         $excludePanelPages = array_map(fn($x) => "$panel/$x.php", [
