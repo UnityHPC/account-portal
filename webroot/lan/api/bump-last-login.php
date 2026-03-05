cb1e98e3069de0b29d7e2d9ed115e7c53980ce4f
Simon Leary <simon.leary42@proton.me>
2026-03-05 14:43:46 -0500

    HTTP POST only

--- webroot/lan/api/bump-last-login.php
+++ webroot/lan/api/bump-last-login.php
@@ -4,6 +4,9 @@ require_once __DIR__ . "/../../../resources/autoload.php";
 
 use UnityWebPortal\lib\UnityHTTPD;
 
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    UnityHTTPD::badRequest("invalid request method {$_SERVER['REQUEST_METHOD']}");
+}
 UnityHTTPD::validateAPIKey();
 $uid = UnityHTTPD::getQueryParameter("uid");
 // please remove this ugly hack https://github.com/UnityHPC/account-portal/pull/593
