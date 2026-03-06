d07fb3d5d42e9959e7fd4662a7d290132a45203a
Simon Leary <simon.leary42@proton.me>
2026-03-06 08:49:33 -0500

    dont forget htmlspecialchars

--- resources/lib/UnityHTTPD.php
+++ resources/lib/UnityHTTPD.php
@@ -70,7 +70,7 @@ class UnityHTTPD
         $errorid = uniqid();
         $title = htmlspecialchars($user_message_title);
         if (trim($user_message_body) !== "") {
-            $body_lines = explode("\n", $user_message_body);
+            $body_lines = explode("\n", htmlspecialchars($user_message_body));
         } else {
             $body_lines = [];
         }
