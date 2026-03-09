2be2798544099fdd353d1224e2829c9ad8e1ccf5
Georgia Stuart <georgia.stuart@gmail.com>
2026-03-09 13:45:47 -0400

    Update idlelock email warning
    Signed-off-by: Georgia Stuart <georgia.stuart@gmail.com>

--- resources/mail/user_expiry_idlelock_warning.php
+++ resources/mail/user_expiry_idlelock_warning.php
@@ -1,7 +1,7 @@
 <p>Hello,</p>
 <p>
 <?php
-$this->Subject = "Account Expiration Warning";
+$this->Subject = "Unity HPC Account Expiration Warning";
 
 $idle_days = $data["idle_days"];
 $expiration_date = $data["expiration_date"];
@@ -10,10 +10,17 @@ $portal_hyperlink = getRelativeHyperlink("account portal");
 $policy_hyperlink = formatHyperlink("account policy", CONFIG["site"]["account_expiration_policy_url"]);
 
 echo "
-    Your account is scheduled to be locked on $expiration_date because you have been idle for too long.
-    Upon expiration, you will lose access to Unity HPC Platform services
-    until you reset the inactivity timer by logging in to the $portal_hyperlink.
-    For more information, see our $policy_hyperlink.
+    To maintain an active Unity account, you must verify your institutional credentials on a biannual basis 
+    due to security requirements. To keep your account active, please log in to the $portal_hyperlink before $expiration_date.
+    Upon expiration, you will lose access to Unity HPC Platform services until you reset the inactivity 
+    timer by logging in to the $portal_hyperlink. For more information, see our $policy_hyperlink.
+
+    Currently, the Unity account portal is the only way to verify credentials. 
+    To maintain an active account, all that's needed is logging in to the portal, you don't need to run jobs or otherwise interact with Unity.
+    To see your current status, you can run unity-account-expiry-status from the Unity command line, or by logging in to the $portal_hyperlink.
+
+    If you are a PI on Unity, please use this opportunity to verify all users under your PI group are current students or collaborators by 
+    visiting the \"My Users\" tab in the $portal_hyperlink. 
 ";
 if ($is_final_warning) {
     echo "This is the final warning.\n";
