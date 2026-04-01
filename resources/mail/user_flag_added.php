0d9ce1a3a73fd6938ac4493089870cbb50df149e
Simon Leary <simon.leary42@proton.me>
2026-04-01 13:36:39 -0400

    show username in user flag emails

--- resources/mail/user_flag_added.php
+++ resources/mail/user_flag_added.php
@@ -2,13 +2,14 @@
 use UnityWebPortal\lib\UserFlag;
 $portal_hyperlink = getRelativeHyperlink("account portal");
 $policy_hyperlink = formatHyperlink("account expiration policy", CONFIG["site"]["account_expiration_policy_url"]);
+$uid = $data["user"];
 switch ($data["flag"]):
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 case UserFlag::QUALIFIED: ?>
 <?php $this->Subject = "User Qualified"; ?>
 <p>Hello,</p>
 <p>
-    Your account on the Unity HPC Platform has been qualified.
+    Your account on the Unity HPC Platform, "<?php echo $uid ?>", has been qualified.
     You should now be able to access Unity HPC Platform services.
     Your account details are below:
 </p>
@@ -30,7 +31,7 @@ page in our documentation for next steps.
 <?php $this->Subject = "User Disabled"; ?>
 <p>Hello,</p>
 <p>
-    Your account on the Unity HPC Platform has been disabled.
+    Your account on the Unity HPC Platform, "<?php echo $uid ?>", has been disabled.
     You should no longer be able to access Unity HPC Platform services.
     This can happen as a result of the <?php echo $policy_hyperlink ?>.
 </p>
@@ -42,7 +43,7 @@ page in our documentation for next steps.
 <?php $this->Subject = "User Locked"; ?>
 <p>Hello,</p>
 <p>
-    Your account on the Unity HPC Platform has been locked.
+    Your account on the Unity HPC Platform, "<?php echo $uid ?>", has been locked.
     You should no longer be able to access Unity HPC Platform services.
 </p>
 <p>If you believe this to be a mistake, please reply to this email as soon as possible.</p>
@@ -53,7 +54,7 @@ page in our documentation for next steps.
 <?php $this->Subject = "User Locked"; ?>
 <p>Hello,</p>
 <p>
-    Your account on the Unity HPC Platform has been locked due to inactivity.
+    Your account on the Unity HPC Platform, "<?php echo $uid ?>", has been locked due to inactivity.
     You should no longer be able to access Unity HPC Platform services.
     To unlock your account, simply log in to the <?php echo $portal_hyperlink ?>.
     See the <?php echo $policy_hyperlink ?> for more information.
@@ -65,7 +66,7 @@ page in our documentation for next steps.
 <?php case UserFlag::ADMIN: ?>
 <?php $this->Subject = "User Promoted"; ?>
 <p>Hello,</p>
-<p>Your account on the Unity HPC Platform has been promoted to admin.</p>
+<p>Your account on the Unity HPC Platform, "<?php echo $uid ?>", has been promoted to admin.</p>
 <p>If you believe this to be a mistake, please reply to this email as soon as possible.</p>
 <?php break; ?>
 
@@ -73,7 +74,7 @@ page in our documentation for next steps.
 <?php case UserFlag::IMMORTAL: ?>
 <?php $this->Subject = "User Immortalized"; ?>
 <p>Hello,</p>
-<p>Your account on the Unity HPC Platform has been made immortal (exempt from expiry).</p>
+<p>Your account on the Unity HPC Platform, "<?php echo $uid ?>", has been made immortal (exempt from expiry).</p>
 <p>If you believe this to be a mistake, please reply to this email as soon as possible.</p>
 <?php break; ?>
 
--- resources/mail/user_flag_removed.php
+++ resources/mail/user_flag_removed.php
@@ -1,12 +1,13 @@
 <?php
 use UnityWebPortal\lib\UserFlag;
+$uid = $data["user"];
 switch ($data["flag"]):
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 case UserFlag::QUALIFIED: ?>
 <?php $this->Subject = "User Disqualified"; ?>
 <p>Hello,</p>
 <p>
-    Your account on the Unity HPC Platform has been disqualified.
+    Your account on the Unity HPC Platform, "<?php echo $uid ?>" has been disqualified.
     You should no longer be able to access Unity HPC Platform services.
 </p>
 <p>In order to be qualified, you must be a PI or be a member of at least one PI group.</p>
@@ -18,7 +19,7 @@ case UserFlag::QUALIFIED: ?>
 <?php $this->Subject = "User Re-Enabled"; ?>
 <p>Hello,</p>
 <p>
-    Your account on the Unity HPC Platform has been re-enabled.
+    Your account on the Unity HPC Platform, "<?php echo $uid ?>" has been re-enabled.
     You should now be able to access Unity HPC Platform services.
 </p>
 <p>If you believe this to be a mistake, please reply to this email as soon as possible.</p>
@@ -29,7 +30,7 @@ case UserFlag::QUALIFIED: ?>
 <?php $this->Subject = "User Unlocked"; ?>
 <p>Hello,</p>
 <p>
-    Your account on the Unity HPC Platform has been unlocked.
+    Your account on the Unity HPC Platform, "<?php echo $uid ?>" has been unlocked.
     You should now be able to access Unity HPC Platform services.
 </p>
 <p>If you believe this to be a mistake, please reply to this email as soon as possible.</p>
@@ -40,7 +41,7 @@ case UserFlag::QUALIFIED: ?>
 <?php $this->Subject = "User Unlocked"; ?>
 <p>Hello,</p>
 <p>
-    Your account on the Unity HPC Platform has been unlocked.
+    Your account on the Unity HPC Platform, "<?php echo $uid ?>" has been unlocked.
     You should now be able to access Unity HPC Platform services.
 </p>
 <p>If you believe this to be a mistake, please reply to this email as soon as possible.</p>
@@ -50,7 +51,7 @@ case UserFlag::QUALIFIED: ?>
 <?php case UserFlag::ADMIN: ?>
 <?php $this->Subject = "User Demoted"; ?>
 <p>Hello,</p>
-<p>Your account on the Unity HPC Platform has been demoted from admin.</p>
+<p>Your account on the Unity HPC Platform, "<?php echo $uid ?>" has been demoted from admin.</p>
 <p>If you believe this to be a mistake, please reply to this email as soon as possible.</p>
 <?php break; ?>
 
@@ -58,7 +59,7 @@ case UserFlag::QUALIFIED: ?>
 <?php case UserFlag::IMMORTAL: ?>
 <?php $this->Subject = "User Mortalized"; ?>
 <p>Hello,</p>
-<p>Your account on the Unity HPC Platform has been made mortal (no longer exempt frome expiry).</p>
+<p>Your account on the Unity HPC Platform, "<?php echo $uid ?>" has been made mortal (no longer exempt frome expiry).</p>
 <p>If you believe this to be a mistake, please reply to this email as soon as possible.</p>
 <?php break; ?>
 
