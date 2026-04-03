ade52d54a11485812d5650e8fb85beac43f4dd58
Simon Leary <simon.leary42@proton.me>
2026-04-03 11:26:50 -0400

    construct a new PHPMailer object for each message

--- resources/lib/UnityMailer.php
+++ resources/lib/UnityMailer.php
@@ -9,7 +9,7 @@ use Twig\TwigFunction;
 /**
  * This is a class that uses PHPmailer to send emails based on templates
  */
-class UnityMailer extends PHPMailer
+class UnityMailer
 {
     private string $MSG_SENDER_EMAIL;
     private string $MSG_SENDER_NAME;
@@ -25,9 +25,6 @@ class UnityMailer extends PHPMailer
 
     public function __construct()
     {
-        parent::__construct(exceptions: true);
-        $this->isSMTP();
-
         $this->MSG_SENDER_EMAIL = CONFIG["mail"]["sender"];
         $this->MSG_SENDER_NAME = CONFIG["mail"]["sender_name"];
         $this->MSG_SUPPORT_EMAIL = CONFIG["mail"]["support"];
@@ -36,46 +33,6 @@ class UnityMailer extends PHPMailer
         $this->MSG_ADMIN_NAME = CONFIG["mail"]["admin_name"];
         $this->MSG_PI_APPROVAL_EMAIL = CONFIG["mail"]["pi_approve"];
         $this->MSG_PI_APPROVAL_NAME = CONFIG["mail"]["pi_approve_name"];
-        if (empty(CONFIG["smtp"]["host"])) {
-            throw new Exception("SMTP server hostname not set");
-        }
-        $this->Host = CONFIG["smtp"]["host"];
-
-        if (empty(CONFIG["smtp"]["port"])) {
-            throw new Exception("SMTP server port not set");
-        }
-        $this->Port = CONFIG["smtp"]["port"];
-
-        $security = CONFIG["smtp"]["security"];
-        $security_conf_valid = empty($security) || $security == "tls" || $security == "ssl";
-        if (!$security_conf_valid) {
-            throw new Exception(
-                "SMTP security is not set correctly, leave empty, use 'tls', or 'ssl'",
-            );
-        }
-        $this->SMTPSecure = $security;
-
-        if (!empty(CONFIG["smtp"]["user"])) {
-            $this->SMTPAuth = true;
-            $this->Username = CONFIG["smtp"]["user"];
-        } else {
-            $this->SMTPAuth = false;
-        }
-
-        if (!empty(CONFIG["smtp"]["pass"])) {
-            $this->Password = CONFIG["smtp"]["pass"];
-        }
-
-        if (CONFIG["smtp"]["ssl_verify"] == "false") {
-            $this->SMTPOptions = [
-                "ssl" => [
-                    "verify_peer" => false,
-                    "verify_peer_name" => false,
-                    "allow_self_signed" => true,
-                ],
-            ];
-        }
-
         $this->loader = new \Twig\Loader\FilesystemLoader([
             __DIR__ . "/../../deployment/mail_overrides",
             __DIR__ . "/../mail",
@@ -93,24 +50,51 @@ class UnityMailer extends PHPMailer
         $this->twig->addGlobal("CONFIG", CONFIG);
     }
 
-    private function resetMessageState(): void
+    public function constructPHPMailer(): PHPMailer
     {
-        $this->clearAllRecipients();
-        $this->clearAttachments();
-        $this->clearCustomHeaders();
-        $this->CharSet = self::CHARSET_ISO88591;
-        $this->ContentType = self::CONTENT_TYPE_PLAINTEXT;
-        $this->Encoding = self::ENCODING_8BIT;
-        $this->From = "";
-        $this->FromName = "";
-        $this->Sender = "";
-        $this->Subject = "";
-        $this->Body = "";
-        $this->AltBody = "";
-        $this->Ical = "";
-        $this->ConfirmReadingTo = "";
-        $this->MessageID = "";
-        $this->MessageDate = "";
+        $mailer = new PHPMailer(exceptions: true);
+        $mailer->isSMTP();
+
+        if (empty(CONFIG["smtp"]["host"])) {
+            throw new Exception("SMTP server hostname not set");
+        }
+        $mailer->Host = CONFIG["smtp"]["host"];
+
+        if (empty(CONFIG["smtp"]["port"])) {
+            throw new Exception("SMTP server port not set");
+        }
+        $mailer->Port = CONFIG["smtp"]["port"];
+
+        $security = CONFIG["smtp"]["security"];
+        $security_conf_valid = empty($security) || $security == "tls" || $security == "ssl";
+        if (!$security_conf_valid) {
+            throw new Exception(
+                "SMTP security is not set correctly, leave empty, use 'tls', or 'ssl'",
+            );
+        }
+        $mailer->SMTPSecure = $security;
+
+        if (!empty(CONFIG["smtp"]["user"])) {
+            $mailer->SMTPAuth = true;
+            $mailer->Username = CONFIG["smtp"]["user"];
+        } else {
+            $mailer->SMTPAuth = false;
+        }
+
+        if (!empty(CONFIG["smtp"]["pass"])) {
+            $mailer->Password = CONFIG["smtp"]["pass"];
+        }
+
+        if (CONFIG["smtp"]["ssl_verify"] == "false") {
+            $mailer->SMTPOptions = [
+                "ssl" => [
+                    "verify_peer" => false,
+                    "verify_peer_name" => false,
+                    "allow_self_signed" => true,
+                ],
+            ];
+        }
+        return $mailer;
     }
 
     /**
@@ -120,29 +104,26 @@ class UnityMailer extends PHPMailer
     public function sendMail(string|array $recipients, string $template, ?array $data = null): void
     {
         $data ??= [];
-        $this->setFrom($this->MSG_SENDER_EMAIL, $this->MSG_SENDER_NAME);
-        $this->addReplyTo($this->MSG_SUPPORT_EMAIL, $this->MSG_SUPPORT_NAME);
+        $mailer = $this->constructPHPMailer();
+        $mailer->setFrom($this->MSG_SENDER_EMAIL, $this->MSG_SENDER_NAME);
+        $mailer->addReplyTo($this->MSG_SUPPORT_EMAIL, $this->MSG_SUPPORT_NAME);
 
         $mes_html = $this->twig->render("$template.html.twig", $data);
-        $this->msgHTML($mes_html);
+        $mailer->msgHTML($mes_html);
 
         if ($recipients == "admin") {
-            $this->addBCC($this->MSG_ADMIN_EMAIL, $this->MSG_ADMIN_NAME);
+            $mailer->addBCC($this->MSG_ADMIN_EMAIL, $this->MSG_ADMIN_NAME);
         } elseif ($recipients == "pi_approve") {
-            $this->addBCC($this->MSG_PI_APPROVAL_EMAIL, $this->MSG_PI_APPROVAL_NAME);
+            $mailer->addBCC($this->MSG_PI_APPROVAL_EMAIL, $this->MSG_PI_APPROVAL_NAME);
         } else {
             if (is_array($recipients)) {
                 foreach ($recipients as $addr) {
-                    $this->addBCC($addr);
+                    $mailer->addBCC($addr);
                 }
             } else {
-                $this->addAddress($recipients);
+                $mailer->addAddress($recipients);
             }
         }
-        try {
-            parent::send();
-        } finally {
-            $this->resetMessageState();
-        }
+        $mailer->send();
     }
 }
