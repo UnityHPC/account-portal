ea90713115ab1b0496d3113c17ac2e4c4a2d5b42
Simon Leary <simon.leary42@proton.me>
2026-04-06 11:09:05 -0400

    fix require

--- workers/configtest.php
+++ workers/configtest.php
@@ -1,6 +1,6 @@
 #!/usr/bin/env php
 <?php
 require_once __DIR__ . "/../resources/lib/utils.php";
-require_once __DIR__ . "/../resources/lib/UnityConfig.php";
+require_once __DIR__ . "/../resources/lib/UnityDeployment.php";
 require_once __DIR__ . "/../resources/config.php";
 
