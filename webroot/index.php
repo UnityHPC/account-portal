9617104994b11e64686cf1e2b6264b799db483d9
Simon Leary <simon.leary42@proton.me>
2026-04-06 10:01:54 -0400

    remove newlines

--- webroot/index.php
+++ webroot/index.php
@@ -1,8 +1,6 @@
 <?php
 
 require_once __DIR__ . "/../resources/autoload.php";
-
-
 require getTemplatePath("header.php");
 require getTemplatePath("home.php");
 require getTemplatePath("footer.php");
