a2b8e44f246dac4bec8df0b334e80975198be7e8
Simon Leary <simon.leary42@proton.me>
2026-04-06 11:11:35 -0400

    simplify configtest

--- workers/configtest.php
+++ workers/configtest.php
@@ -1,6 +1,2 @@
 #!/usr/bin/env php
-<?php
-require_once __DIR__ . "/../resources/lib/utils.php";
-require_once __DIR__ . "/../resources/lib/UnityDeployment.php";
-require_once __DIR__ . "/../resources/config.php";
-
+<?php include __DIR__ . "/init.php";
