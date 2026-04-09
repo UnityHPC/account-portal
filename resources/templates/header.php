e52410b811c281f422f7f6e804c05a13d25ccef5
Simon Leary <simon.leary42@proton.me>
2026-04-09 09:01:33 -0400

    don't expose the entire $_SESSION

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -37,4 +37,8 @@ if (isset($SSO)) {
     }
 }
 
-echo $TWIG->render("header.html.twig", ["_SESSION" => $_SESSION]);
+echo $TWIG->render("header.html.twig", [
+    "user_exists" => $_SESSION["user_exists"] ?? false,
+    "is_pi" => $_SESSION["is_pi"] ?? false,
+    "is_admin" => $_SESSION["is_admin"] ?? false
+]);
--- resources/templates/home.php
+++ resources/templates/home.php
@@ -1,3 +1,3 @@
 <?php
 
-echo $TWIG->render("home.html.twig", ["_SESSION" => $_SESSION]);
+echo $TWIG->render("home.html.twig", ["user_exists" => $_SESSION["user_exists"] ?? false]);
