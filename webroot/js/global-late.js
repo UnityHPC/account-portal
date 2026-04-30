5e240a140785fb7523ae9f4885ddb050afdc4fec
Simon Leary <simon.leary42@proton.me>
2026-04-30 14:18:47 -0400

    switch navbar to popover

--- webroot/js/global-late.js
+++ webroot/js/global-late.js
@@ -1,34 +1,4 @@
 (function () {
-  $(document).ready(setNav);
-  $(window).resize(setNav);
-
-  function setNav() {
-    if ($("button.hamburger").is(":visible")) {
-      $("nav.mainNav").hide(); // Mobile View
-      $("button.hamburger").attr("aria-expanded", "false");
-    } else {
-      $("nav.mainNav").show(); // Desktop View
-      $("button.hamburger").attr("aria-expanded", "true");
-    }
-  }
-
-  $("button.hamburger").on("click", function () {
-    $("nav.mainNav").toggle();
-    var expanded_before = $("button.hamburger").attr("aria-expanded");
-    var expanded_after = expanded_before === "true" ? "false" : "true";
-    $("button.hamburger").attr("aria-expanded", expanded_after);
-  });
-
-  $("main").click(function (e) {
-    if (
-      !$(e.target).parent().hasClass("hamburger") &&
-      $("button.hamburger").is(":visible")
-    ) {
-      $("button.hamburger").attr("aria-expanded", "false");
-      $("nav.mainNav").hide();
-    }
-  });
-
   // Functions to set nav links as active. Sub links can activate parents by naming files with same prefix, for example: documentation.php and documentation_view.php activate the same link
   var url = location.pathname;
   if (url.lastIndexOf(".") >= 0) {
