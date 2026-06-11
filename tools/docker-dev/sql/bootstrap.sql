5efadf2b25c0f245e9070747f7d67149cae35d2c
Simon Leary <simon.leary42@proton.me>
2026-06-11 13:08:48 -0400

    remove comments from bootstrap.sql

--- tools/docker-dev/sql/bootstrap.sql
+++ tools/docker-dev/sql/bootstrap.sql
@@ -1,32 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.9.5deb2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 13, 2023 at 02:29 AM
--- Server version: 10.3.38-MariaDB-0ubuntu0.20.04.1
--- PHP Version: 7.4.3-4ubuntu2.19
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `unity`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `audit_log`
---
 
 CREATE TABLE `audit_log` (
   `id` int(11) NOT NULL,
@@ -37,22 +12,10 @@ CREATE TABLE `audit_log` (
   `recipient` varchar(768) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `user_last_logins`
---
-
 CREATE TABLE user_last_logins (
    operator VARCHAR(768) PRIMARY KEY,
    last_login TIMESTAMP
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `requests`
---
 
 CREATE TABLE `requests` (
   `id` int(11) NOT NULL,
@@ -61,40 +24,14 @@ CREATE TABLE `requests` (
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `audit_log`
---
 ALTER TABLE `audit_log`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `requests`
---
 ALTER TABLE `requests`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `audit_log`
---
 ALTER TABLE `audit_log`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `requests`
---
 ALTER TABLE `requests`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1031;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
