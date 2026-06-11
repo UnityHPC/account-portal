90181509019f5356b384c7ba5f90e91329538296
Simon Leary <71396965+simonLeary42@users.noreply.github.com>
2026-06-11 15:29:06 -0400

    SQL housekeeping 2 (#743)

--- tools/docker-dev/sql/bootstrap.sql
+++ tools/docker-dev/sql/bootstrap.sql
@@ -4,7 +4,7 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 CREATE TABLE `audit_log` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
   `operator` varchar(128) NOT NULL,
   `operator_ip` varchar(15) NOT NULL,
@@ -12,26 +12,14 @@ CREATE TABLE `audit_log` (
   `recipient` varchar(768) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE user_last_logins (
-   operator VARCHAR(768) PRIMARY KEY,
-   last_login TIMESTAMP
+CREATE TABLE `user_last_logins` (
+   `operator` VARCHAR(768) PRIMARY KEY,
+   `last_login` TIMESTAMP
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 CREATE TABLE `requests` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `request_for` varchar(131) NOT NULL,
   `uid` varchar(128) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-ALTER TABLE `audit_log`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `requests`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `audit_log`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `requests`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1031;
