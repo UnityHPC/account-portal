0a65a26871126394d3720c07cf7efc73f3708f14
Simon Leary <71396965+simonLeary42@users.noreply.github.com>
2026-07-15 14:14:19 -0400

    move composer, npm inside the `web` container (#755)

--- tools/docker-dev/build.sh
+++ tools/docker-dev/build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-set -e
+set -euo pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+
 cd "$(dirname "$0")"
+
+# docker won't let you copy files from the parent directory of the dockerfile
+# another possible workaround is to use the `context` option in docker compose
+cp ../../composer.json web/composer.json
+cp ../../composer.lock web/composer.lock
+cp ../../package.json web/package.json
+cp ../../package-lock.json web/package-lock.json
+
 docker compose down
 docker compose build
 # docker image prune
