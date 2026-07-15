f2d7cde423fbfde20d6a011d8bf02fecd27c519c
Simon Leary <simon.leary42@proton.me>
2026-07-15 14:11:19 -0400

    update comment

--- tools/docker-dev/build.sh
+++ tools/docker-dev/build.sh
@@ -5,7 +5,7 @@ trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 cd "$(dirname "$0")"
 
 # docker won't let you copy files from the parent directory of the dockerfile
-# one workaround is to use the `context` option in docker compose
+# another possible workaround is to use the `context` option in docker compose
 cp ../../composer.json web/composer.json
 cp ../../composer.lock web/composer.lock
 cp ../../package.json web/package.json
