667c79fd049172f468a4ccd16f05f183313401aa
Simon Leary <simon.leary42@proton.me>
2026-07-15 10:12:24 -0400

    add CI to assert upstream version bumped

---
+++ test/assert-upstream-version-bumped.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -euo pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+
+if [[ $# -ne 2 ]]; then
+    echo "usage: $0 <base-ref> <head-ref>"
+    exit 1
+fi
+
+base_ref="$1"
+head_ref="$2"
+
+if [[ "$base_ref" =~ ^0+$ ]]; then
+    base_ref="$(git rev-list --max-parents=0 "$head_ref" | tail -n 1)"
+fi
+
+changed_files="$(git diff --name-only "$base_ref" "$head_ref")"
+
+if ! grep -Eq '^(webroot/css/|webroot/js/)' <<< "$changed_files"; then
+    exit 0
+fi
+
+version_base="$(git show "$base_ref:deployment/config.base.ini" | grep 'Current upstream version')"
+version_head="$(git show "$head_ref:deployment/config.base.ini" | grep 'Current upstream version')"
+if [ "$version_base" == "$version_head" ]; then
+    echo "webroot/css or webroot/js changed, but deployment/config.base.ini upstream.version was not updated"
+    exit 1
+fi
