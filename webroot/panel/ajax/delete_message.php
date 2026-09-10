c48e0289d9742dcae9096080bec5479c640fa09a
Simon Leary <71396965+simonLeary42@users.noreply.github.com>
2026-09-10 16:30:00 -0400

    deleting a nonexistent message is a warning (#769)

--- webroot/panel/ajax/delete_message.php
+++ webroot/panel/ajax/delete_message.php
@@ -4,10 +4,15 @@ require_once __DIR__ . "/../../../resources/autoload.php";
 
 use UnityWebPortal\lib\UnityHTTPD;
 use UnityWebPortal\lib\UnityHTTPDMessageLevel;
+use UnityWebPortal\lib\exceptions\UnityHTTPDMessageNotFoundException;
 
 $level_str = _base64_decode(UnityHTTPD::getPostData("level"));
 $level = UnityHTTPDMessageLevel::from($level_str);
 $title = _base64_decode(UnityHTTPD::getPostData("title"));
 $body = _base64_decode(UnityHTTPD::getPostData("body"));
-UnityHTTPD::deleteMessage($level, $title, $body);
+try {
+    UnityHTTPD::deleteMessage($level, $title, $body);
+} catch (UnityHTTPDMessageNotFoundException $e) {
+    UnityHTTPD::errorLog("warning", "deleted nonexistent message", error: $e);
+}
 UnityHTTPD::die();
